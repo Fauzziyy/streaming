8910d3a8617f6f64d3eabad5fbf496f07adefdd9
Fauzziyy <alfauzinabil@gmail.com>
2023-09-26 14:03:26 +0700

    first commit

---
+++ application/views/login/template/footer.php
@@ -0,0 +1,13 @@
+   <!-- /.login-card-body -->
+   </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="<?= base_url() ?>assets/Admin/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= base_url() ?>assets/Admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url() ?>assets/Admin/dist/js/adminlte.min.js"></script>
+</body>
+</html>
